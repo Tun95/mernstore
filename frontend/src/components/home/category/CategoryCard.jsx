1fc0dc84fcd6a9af39a1691a9ddf5286e9ec5ae4
Ola Josh <akandetunji3@gmail.com>
2023-10-10 18:04:44 +0100

    new

--- frontend/src/components/home/category/CategoryCard.jsx
+++ frontend/src/components/home/category/CategoryCard.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CategoryCard({ data }) {
   return (
     <div className="category_card">
       {data.map((item, index) => (
-        <div className="category_content p_flex" key={index}>
+        <Link to="/store" className="category_content p_flex" key={index}>
           <div className="cards">
             <img src={item.img} alt="" className="img" />
             <p>{item.name}</p>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
